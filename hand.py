7356bb43e06949a184caf5d88c7c613b9e064753
Swarit Choudhari <68472469+PythonSwarit@users.noreply.github.com>
2020-07-24 09:43:59 -0400

    Create hand.py

---
+++ hand.py
@@ -0,0 +1,78 @@
+import turtle
+
+a = 5
+print(a)
+
+b = "hello"
+print(b)
+
+credit_card = 133777799559990
+
+print(credit_card)
+
+qazi_turtle = turtle.Turtle()
+qazi_turtle.speed(20000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000)
+
+# Square
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+
+def Square():
+   qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+
+Square()
+qazi_turtle.forward(30)
+Square()
+qazi_turtle.right(60)
+qazi_turtle.forward(200)
+qazi_turtle.right(90)
+qazi_turtle.forward(200)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.forward(60)
+qazi_turtle.left(35)
+qazi_turtle.forward(84)
+qazi_turtle.left(90)
+qazi_turtle.left(88)
+qazi_turtle.forward(90)
+qazi_turtle.right(90)
+Square()
+qazi_turtle.left(110)
+qazi_turtle.forward(104)
+qazi_turtle.right(52)
+qazi_turtle.forward(36)
+qazi_turtle.left(90)
+qazi_turtle.forward(50)
+qazi_turtle.left(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(180)
+qazi_turtle.forward(100)
+qazi_turtle.left(90)
+qazi_turtle.forward(50)
+qazi_turtle.left(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(180)
+qazi_turtle.forward(100)
+qazi_turtle.left(90)
+qazi_turtle.forward(50)
+qazi_turtle.left(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(180)
+qazi_turtle.forward(100)
+qazi_turtle.left(90)
+qazi_turtle.forward(50)
+qazi_turtle.left(90)
+qazi_turtle.forward(200)
+qazi_turtle.left(59)
+qazi_turtle.forward(330)
